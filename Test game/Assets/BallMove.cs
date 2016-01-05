949458679edfa4ff45a6d6b8e346e5c24ab72653
Tmutsaers <tim.mutsaers@gmail.com>
2016-01-05 16:19:15 +0100

    First Commit

---
+++ Test game/Assets/BallMove.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMove : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody body;
+
+    // Use this for initialization
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+
+        speed = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        body.AddForce(movement * speed);
+
+    }
+}
